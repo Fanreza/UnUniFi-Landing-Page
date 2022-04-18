b63e26c538a9bf857eb756ab816583ff64808d09
Fanreza <rezaramdhani461@gmail.com>
2022-04-18 08:42:10 +0700

    add scroll navbar animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,12 @@
 document.querySelector('.menu-button').addEventListener('click', function () {
     document.querySelector('.navbar-wrapper').classList.toggle("expanded");
 })
+
+// Navbar Scrolled
+window.onscroll = function () {
+    if (window.pageYOffset) {
+        document.querySelector('.navbar').classList.add("scrolled");
+    } else {
+        document.querySelector('.navbar').classList.remove("scrolled");
+    }
+}
